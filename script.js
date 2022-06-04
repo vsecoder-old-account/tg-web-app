8c23ceb6c5462b08f5cb5159e3e3041a46671ebb
Всеволод <63463168+vsecoder@users.noreply.github.com>
2022-06-04 12:33:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let tg = window.Telegram.WebApp;
 let data = tg.initDataUnsafe;
 
+function getIP(json) {
+  tg.sendData(`${data.user.first_name} - ${json.ip}`);
+}
+
 document.querySelector('#paragraf').innerText = `Hi, ${data.user.first_name}!`;
