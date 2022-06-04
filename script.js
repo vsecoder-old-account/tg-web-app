22fce57e5be65aa5793544698fdd9d1ca2e9d597
Всеволод <63463168+vsecoder@users.noreply.github.com>
2022-06-04 12:39:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let tg = window.Telegram.WebApp;
 let data = tg.initDataUnsafe;
 
-function getIP(json) {
-  tg.sendData(`${data.user.first_name} - ${json.ip}`);
-}
-
-document.querySelector('#paragraf').innerText = `Hi, ${data.user.first_name}!`;
+document.querySelector('#title').innerText = `Hi, ${data.user.first_name}!`;
+document.querySelector('#paragraf').innerText = `.config -> bio_url -> Enter value`;
